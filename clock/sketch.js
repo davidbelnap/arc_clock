718951549571016f069e472065833cf6835716ff
David Belnap <david.belnap6@gmail.com>
2017-11-16 17:56:53 -0700

    fit canvas to window, adjust clock size

--- clock/sketch.js
+++ clock/sketch.js
@@ -1,33 +1,33 @@
 function setup() {
-  createCanvas(400, 400)
+  createCanvas(window.innerWidth, window.innerHeight);
   angleMode(DEGREES);
 }
 
 function draw() {
 	background(0);
-  translate(200, 200);
+  translate(canvas.width/2, canvas.height/2);
   rotate(-90);
 
 	let hr = hour();
 	let mn = minute();
 	let sc = second();
 
-	strokeWeight(8);
+	strokeWeight(12);
 	stroke('#50D0D6');
 	noFill();
 	let scAngle = map(sc, 0, 60, 0, 360);
-	arc(0, 0, 300, 300, 0, scAngle);
+	arc(0, 0, 450, 450, 0, scAngle);
 
-	strokeWeight(8);
+	strokeWeight(12);
 	stroke('#F486BC');
 	noFill();
 	let mnAngle = map(mn, 0, 60, 0, 360);
-	arc(0, 0, 280, 280, 0, mnAngle);
+	arc(0, 0, 420, 420, 0, mnAngle);
 
-	strokeWeight(8);
+	strokeWeight(12);
 	stroke('#D252DB');
 	noFill();
 	let hrAngle = map(hr % 12, 0, 12, 0, 360);
-	arc(0, 0, 260, 260, 0, hrAngle);
+	arc(0, 0, 390, 390, 0, hrAngle);
 
 }
