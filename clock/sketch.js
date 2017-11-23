874c4241023e8e764353b7e55ca6de7fefcf3256
David Belnap <david.belnap6@gmail.com>
2017-11-22 23:52:03 -0700

    add sc, mn, hr hands to interface

--- clock/sketch.js
+++ clock/sketch.js
@@ -1,7 +1,7 @@
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   angleMode(DEGREES);
-}
+};
 
 function draw() {
 	background(0);
@@ -18,11 +18,23 @@ function draw() {
 	let scAngle = map(sc, 0, 60, 0, 360);
 	arc(0, 0, 450, 450, 0, scAngle);
 
+	push();
+	rotate(scAngle);
+	stroke('#50D0D6');
+	line(0, 0, 170, 0);
+	pop();
+
 	strokeWeight(12);
 	stroke('#F486BC');
 	noFill();
 	let mnAngle = map(mn, 0, 60, 0, 360);
 	arc(0, 0, 420, 420, 0, mnAngle);
+
+	push();
+	rotate(mnAngle);
+	stroke('#F486BC');
+	line(0, 0, 150, 0);
+	pop();
 
 	strokeWeight(12);
 	stroke('#D252DB');
@@ -30,4 +42,9 @@ function draw() {
 	let hrAngle = map(hr % 12, 0, 12, 0, 360);
 	arc(0, 0, 390, 390, 0, hrAngle);
 
-}
+	push();
+	rotate(hrAngle);
+	stroke('#D252DB');
+	line(0, 0, 110, 0);
+	pop();
+};
